356a2a089279bb86f3f1f9f085e03ad397afa7aa
sawfly29 <sawfly@yandex.ru>
2021-02-15 20:58:21 +0300

    up to 47.30 (connected react and redux)

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import {connect} from 'react-redux'//соединение реакта и редакса
 import Post from "./Post";
 
 // eslint-disable-next-line import/no-anonymous-default-export
- const Posts = ({ posts }) => {
-    if (!posts.length){return <h3 className='text-center'>No posts here yet!</h3>}
-  return posts.map((post) => <Post post={post} key={post} />);
+ const Posts = ({ syncPosts }) => {
+    if (!syncPosts.length){return <h3 className='text-center'>No posts here yet!</h3>}
+  return syncPosts.map((post) => <Post post={post} key={post} />);
 };
 
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    syncPosts: state.posts.posts
+  }
+}//преобразователь state в пропсы
 
-export default connect(null, null)(Posts)
+export default connect(mapStateToProps, null)(Posts)
--- src/redux/postsReducer.js
+++ src/redux/postsReducer.js
@@ -1,10 +1,20 @@
+import {CREATE_POST} from './types'
 const initialState = {
     posts: [],
     fetchedPosts: []
 }
 
 export const postsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CREATE_POST:
+            return {...state, posts: [...state.posts, action.payload]};
+            //return {...state, posts: state.posts.concat(action.payload)};
+            //получили новый экшн, по нему вернули предыдущий стейт и добавили пэйлоад (нове данные)
+    
+        default:
+            return state;
+    }
 
 
-    return state
+    
 }
